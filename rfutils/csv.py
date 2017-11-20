70cc0a04db6334ee360bf3ed35d55119bec88bd5
Futrell <futrell@mit.edu>
2017-11-20 14:19:56 -0500

    add csv module

---
+++ rfutils/csv.py
@@ -0,0 +1,14 @@
+import csv
+
+def write_dicts(file, lines):
+    lines_iter = iter(lines)
+    first_line = next(lines_iter)
+    writer = csv.DictWriter(file, first_line.keys())
+    writer.writeheader()
+    writer.writerow(first_line)
+    for line in lines_iter:
+        writer.writerow(line)
+
+        
+    
+    
